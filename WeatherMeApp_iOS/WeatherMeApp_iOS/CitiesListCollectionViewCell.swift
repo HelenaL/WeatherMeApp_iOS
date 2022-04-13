42e7d3fe4a89dd8032e2e523dd54e10ccb19bef1
Elena Shatunova <lissi88@yandex.ru>
2022-04-12 23:03:04 -0400

    cities collectionview add

---
+++ WeatherMeApp_iOS/WeatherMeApp_iOS/CitiesListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CitiesListCollectionViewCell.swift
+//  WeatherMeApp_iOS
+//
+//  Created by Lenochka on 4/12/22.
+//
+
+import UIKit
+
+class CitiesListCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var tempValueLabel: UILabel!
+}
---
+++ WeatherMeApp_iOS/WeatherMeApp_iOS/CitiesListVC.swift
@@ -0,0 +1,66 @@
+//
+//  CitiesListVC.swift
+//  WeatherMeApp_iOS
+//
+//  Created by Lenochka on 4/12/22.
+//
+
+import UIKit
+
+class CitiesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension CitiesListVC: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CitiesListCollectionViewCell", for: indexPath) as! CitiesListCollectionViewCell
+        cell.cityNameLabel.text = String("City \(indexPath.row + 1)")
+        return cell
+    }
+}
+
+extension CitiesListVC: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row + 1)
+    }
+}
+
+extension CitiesListVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        return CGSize(width: collectionView.bounds.size.width - 16, height: 120)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.init(top: 8, left: 8, bottom: 0, right: 8)
+    }
+}
