25ceff26c3b87b7e12e75c492b475cb8694f1e95
Elena Sh <lissi88@yandex.ru>
2023-04-18 23:44:39 -0400

    HourlyWeatherCell ui add

--- WeatherMeApp_iOS/WeatherMeApp_iOS/ViewControllers/CityWeatherVC/HourlyWeatherCell.swift
+++ WeatherMeApp_iOS/WeatherMeApp_iOS/ViewControllers/CityWeatherVC/HourlyWeatherCell.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class HourlyWeatherCell: UICollectionViewCell {
     
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
     
     func cellConfigurate () {
 
