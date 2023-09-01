82a119c033dd64d8b129d7e4079bc3a839a819ff
Elena Sh <lissi88@yandex.ru>
2023-08-31 23:54:43 -0400

    table view instead of collection view on cities list

---
+++ WeatherMeApp_iOS/WeatherMeApp_iOS/ViewControllers/CitiesListVC/CitiesListTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  CitiesListTableViewCell.swift
+//  WeatherMeApp_iOS
+//
+//  Created by Lenochka on 8/31/23.
+//
+
+import Foundation
+import WeatherKit
+import UIKit
+
+class CitiesListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var tempValueLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var tempRangeLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var lineImageView: UIView!
+    
+    func fillWeatherCell(with city:City, and weather: Weather) {
+        lineImageView.layer.cornerRadius = 1.3
+        cityNameLabel.text = city.name
+        
+        tempValueLabel.attributedText = String.tempFormattedString(value: weather.currentWeather.temperature.value, unit: weather.currentWeather.temperature.unit, bFontSize: 36, sFontSize: 26, weight: .semibold)
+        
+        if let alerts = weather.weatherAlerts {
+            if let alertText = alerts.first?.summary {
+                conditionLabel.text = "\u{26A0} " + alertText
+            }
+        } else {
+            conditionLabel.text = weather.currentWeather.condition.description
+        }
+
+        if let tz = city.timeZone {
+            timeLabel.text = Date.utcToLocal(date: weather.currentWeather.date, timezone: tz, with: "h:mm a")
+        }
+        
+        if let dayWeather = weather.dailyForecast.first {
+            tempRangeLabel.text = "H:" + String(format: "%.0f", dayWeather.highTemperature.value) + "º L:" + String(format: "%.0f", dayWeather.lowTemperature.value) + "º"
+        }
+//        tempRangeLabel.text = "H: " + (weather.dailyForecast.forecast.first?.highTemperature.formatted ?? "--") + "L: " + (weather.dailyForecast.forecast.first?.lowTemperature.formatted() ?? "--")
+       // String(format: "%.0f", dayWeather.highTemperature.)
+    }
+    
+}
--- WeatherMeApp_iOS/WeatherMeApp_iOS/ViewControllers/CitiesListVC/CitiesListVC.swift
+++ WeatherMeApp_iOS/WeatherMeApp_iOS/ViewControllers/CitiesListVC/CitiesListVC.swift
@@ -13,7 +13,8 @@ import WeatherKit
 class CitiesListVC: UIViewController {
     
     // MARK: - Properties
-    @IBOutlet weak var collectionView: UICollectionView!
+    //@IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var tableView: UITableView!
     
     var container: NSPersistentContainer = CoreDataStack.shared.persistentContainer
     var cities: [City] = []
@@ -59,83 +60,14 @@ class CitiesListVC: UIViewController {
             //self.collectionView.reloadData()
         }
         
-        
-        var layoutConfig = UICollectionLayoutListConfiguration(appearance: .plain)
-        layoutConfig.showsSeparators = false
-        layoutConfig.trailingSwipeActionsConfigurationProvider = { [unowned self] (indexPath) in
-
-//            guard let item = dataSource.itemIdentifier(for: indexPath) else {
-//                return nil
-//            }
-
-            // 2
-            // Create action 1
-            let action1 = UIContextualAction(style: .normal, title: "Action 1") { (action, view, completion) in
-                print("🥵 Delete this city")
-                // 3
-                // Handle swipe action by showing alert message
-                //handleSwipe(for: action, item: item)
-
-                // 4
-                // Trigger the action completion handler
-                completion(true)
-            }
-
-            action1.backgroundColor = .systemRed
-            return UISwipeActionsConfiguration(actions: [action1])
-        }
-
-
-        
-        let listLayout = UICollectionViewCompositionalLayout.list(using: layoutConfig)
-        collectionView.collectionViewLayout = listLayout
-        //collectionView.collectionViewLayout = createCollectionViewLayout()
-
     }
     
-    private func createLayout() -> UICollectionViewLayout {
-        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-            guard let sectionKind = Section(rawValue: sectionIndex) else { return nil }
-            
-            var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
-            configuration.showsSeparators = false
-            configuration.headerMode = .none
-            configuration.trailingSwipeActionsConfigurationProvider = { [unowned self] (indexPath) in
-
-    //            guard let item = dataSource.itemIdentifier(for: indexPath) else {
-    //                return nil
-    //            }
-
-                // 2
-                // Create action 1
-                let action1 = UIContextualAction(style: .normal, title: "Action 1") { (action, view, completion) in
-                    print("🥵 Delete this city")
-                    completion(true)
-                }
-                action1.backgroundColor = .systemRed
-                return UISwipeActionsConfiguration(actions: [action1])
-            }
-           
-            //                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0) //8
-            return NSCollectionLayoutSection.list(using: configuration, layoutEnvironment: layoutEnvironment)
-        }
-
-        let layout = UICollectionViewCompositionalLayout(sectionProvider: sectionProvider)
-        return layout
-    }
-    
-//    func collectionView(_ collectionView: UICollectionView, canPerformPrimaryActionForItemAt indexPath: IndexPath) -> Bool {
-//        return true
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, canEditItemAt indexPath: IndexPath) -> Bool {
-//        return true
-//    }
     
     
     @objc func newCityAdded(_ notification: Notification) {
         cities = CoreDataStack.shared.getCitiesList()
-        self.collectionView.reloadData()
+        //self.collectionView.reloadData()
+        self.tableView.reloadData()
     }
         
     func getWeatherForCity(_ city: City,
@@ -165,7 +97,7 @@ class CitiesListVC: UIViewController {
     
     func needToReload(_ city: City) -> Bool {
         var wDate: Date?
-        var nowDate = Date.now
+        let nowDate = Date.now
         var diff = 0
         
         if let key = city.placemarkTitle {
@@ -184,7 +116,7 @@ class CitiesListVC: UIViewController {
     // openCityWeather
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let vc = segue.destination as? CityWeatherViewController {
-            if let indexPath = collectionView.indexPathsForSelectedItems?.first {
+            if let indexPath = tableView.indexPathForSelectedRow {
                 let city = cities[indexPath.row]
                 vc.city = (city.name ?? "", city.placemarkTitle ?? "", city.lat, city.long, city.timeZone ?? "EST")
                 vc.isTopButtonHidden = (cancel: true, add: true)
@@ -193,19 +125,24 @@ class CitiesListVC: UIViewController {
     }
 }
 
-// MARK: - Collection View Extensions
-
-extension CitiesListVC: UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+// MARK: - Table View Extensions
+extension CitiesListVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row + 1)
     }
+}
 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+extension CitiesListVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cities.count
     }
-     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CitiesListCollectionViewCell", for: indexPath) as! CitiesListCollectionViewCell
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CitiesListTableViewCell", for: indexPath) as! CitiesListTableViewCell
         let cellCity = cities[indexPath.row]
         
         getWeatherForCity(cellCity) { weather, city in
@@ -216,89 +153,116 @@ extension CitiesListVC: UICollectionViewDataSource {
         
         return cell
     }
-}
-
-extension CitiesListVC: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(indexPath.row + 1)
-    }
-}
-
-extension CitiesListVC: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-
-        return CGSize(width: collectionView.bounds.size.width - 16, height: 128)
-    }
     
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 8
-    }
-
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets.init(top: 8, left: 8, bottom: 0, right: 8)
-    }
-    
-    func createCollectionViewLayout() -> UICollectionViewCompositionalLayout {
-        return UICollectionViewCompositionalLayout { [self] (section, _) -> NSCollectionLayoutSection? in
-            if section == 0 {
-                var layoutConfig = UICollectionLayoutListConfiguration(appearance: .plain)
-                layoutConfig.trailingSwipeActionsConfigurationProvider = { [unowned self] (indexPath) in
-
-        //            guard let item = dataSource.itemIdentifier(for: indexPath) else {
-        //                return nil
-        //            }
-
-                    // Create action 1
-                    let action1 = UIContextualAction(style: .normal, title: "Action 1") { (action, view, completion) in
-                        print("🥵 Delete this city")
-                        completion(true)
-                    }
-
-                    action1.backgroundColor = .systemRed
-                    return UISwipeActionsConfiguration(actions: [action1])
-                }
-
-
-                let item = NSCollectionLayoutItem(
-                    layoutSize: NSCollectionLayoutSize(
-                        widthDimension: .fractionalWidth(1),
-                        heightDimension: .fractionalHeight(1)
-                    )
-                )
-            
-                // group
-                let group = NSCollectionLayoutGroup.horizontal(
-                    layoutSize: NSCollectionLayoutSize(
-                        widthDimension: .fractionalWidth(1),
-                        heightDimension: .absolute(120)
-                    ),
-                    subitem: item,
-                    count: 1
-                )
-                group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-                
-                // section
-                let section = NSCollectionLayoutSection(group: group)
-                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0) //8
-                return section
-
-            }
-            return nil
-        }
-    }
-    
-
     
 }
+
+//// MARK: - Collection View Extensions
+//
+//extension CitiesListVC: UICollectionViewDataSource {
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return 1
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return cities.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CitiesListCollectionViewCell", for: indexPath) as! CitiesListCollectionViewCell
+//        let cellCity = cities[indexPath.row]
+//
+//        getWeatherForCity(cellCity) { weather, city in
+//            cell.fillWeatherCell(with: city, and: weather)
+//        } errorBlock: { city in
+//            cell.timeLabel.text = "something went wrong"
+//        }
+//
+//        return cell
+//    }
+//}
+//
+//extension CitiesListVC: UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        print(indexPath.row + 1)
+//    }
+//}
+//
+//extension CitiesListVC: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        return CGSize(width: collectionView.bounds.size.width - 16, height: 128)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 8
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets.init(top: 8, left: 8, bottom: 0, right: 8)
+//    }
+//
+//    func createCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+//        return UICollectionViewCompositionalLayout { [self] (section, _) -> NSCollectionLayoutSection? in
+//            if section == 0 {
+//                var layoutConfig = UICollectionLayoutListConfiguration(appearance: .plain)
+//                layoutConfig.trailingSwipeActionsConfigurationProvider = { [unowned self] (indexPath) in
+//
+//        //            guard let item = dataSource.itemIdentifier(for: indexPath) else {
+//        //                return nil
+//        //            }
+//
+//                    // Create action 1
+//                    let action1 = UIContextualAction(style: .normal, title: "Action 1") { (action, view, completion) in
+//                        print("🥵 Delete this city")
+//                        completion(true)
+//                    }
+//
+//                    action1.backgroundColor = .systemRed
+//                    return UISwipeActionsConfiguration(actions: [action1])
+//                }
+//
+//
+//                let item = NSCollectionLayoutItem(
+//                    layoutSize: NSCollectionLayoutSize(
+//                        widthDimension: .fractionalWidth(1),
+//                        heightDimension: .fractionalHeight(1)
+//                    )
+//                )
+//
+//                // group
+//                let group = NSCollectionLayoutGroup.horizontal(
+//                    layoutSize: NSCollectionLayoutSize(
+//                        widthDimension: .fractionalWidth(1),
+//                        heightDimension: .absolute(120)
+//                    ),
+//                    subitem: item,
+//                    count: 1
+//                )
+//                group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+//
+//                // section
+//                let section = NSCollectionLayoutSection(group: group)
+//                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0) //8
+//                return section
+//
+//            }
+//            return nil
+//        }
+//    }
+    
+
+    
+//}
