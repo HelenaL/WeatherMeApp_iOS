6d7fe5106a4da7c4f816a32de7b8e4106d35fd07
Elena Sh <lissi88@yandex.ru>
2023-09-25 22:15:07 -0400

    fix

---
+++ WeatherMeApp_iOS/WeatherMeApp_iOS/City+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  City+CoreDataClass.swift
+//  
+//
+//  Created by Lenochka on 7/11/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(City)
+public class City: NSManagedObject {
+
+}
